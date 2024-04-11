d32ed2fe13f713540baefb9033990d79ed4e7d4b
SauLes95 <sestaklaura11@gmail.com>
2024-04-11 10:42:27 +0200

    Implemented Index action in ClientController to display client information. Added view Index.cshtml to show the number of clients using Bootstrap components.

---
+++ Vjezba/Vjezba.Web/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Vjezba.Web.Mock;
+using Vjezba.Web.Models;
+
+namespace Vjezba.Web.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            IQueryable<Client> clientsQuery = MockClientRepository.Instance.All();
+            List<Client> clients = clientsQuery.ToList();
+            var clientNum = clients.Count;
+            ViewBag.Message = $"U sustavu postoji {clientNum} klijenata.";
+
+            return View("index");
+        }
+    }
+}
